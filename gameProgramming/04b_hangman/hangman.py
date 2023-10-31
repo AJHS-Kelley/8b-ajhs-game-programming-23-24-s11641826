1c68fb245eff8bd99e87019b6698fbd7529d7fa6
Albert <s11641826@students.duvalschools.org>
2023-10-31 14:17:26 -0400

    v0.2
    creating hangman board

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,5 +1,36 @@
-# Hangman Game by Albert Laguerre, v0.1
+# Hangman Game by Albert Laguerre, v0.2
 
 words = 'cat cow dog elephant sheep lion snack mouse cheetah bat red blue yellow orange black white purple pink violet silver phytoplanton mitochondria supercalifragilistiexpiealadous Sesquipedalian Antidisestablishmentarianism aintbiotics'
 
+# VARIABLE_NAMES IN ALL-CAPS ARE CONSTANTS AND NOT MEAN TO CHANGE!
+HANGMAN_BOARD = ['''
+    +---+
+        |
+        |           
+        |            
+     =======''', '''   
+    +---+
+    O   |
+        |           
+        |            
+     =======''', '''   
+    +---+
+    O   |
+   /|\  |           
+        |            
+     =======''', '''   
+    +---+
+    O   |
+   /|\  |           
+   /     |            
+     =======''', '''   
+    +---+
+    O   |
+   /|\  |           
+   / \     |            
+     =======''']
 
+i = 0
+while i < len(HANGMAN_BOARD):
+    print(HANGMAN_BOARD[i])
+    i += 1
