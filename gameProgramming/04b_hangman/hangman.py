a197f9ec58448154734e59e39dbadc690dbc44e9
Albert <s11641826@students.duvalschools.org>
2023-10-31 13:58:32 -0400

    v0.1
    adding words

---
+++ gameProgramming/04b_hangman/hangman.py
@@ -0,0 +1,5 @@
+# Hangman Game by Albert Laguerre, v0.1
+
+words = 'cat cow dog elephant sheep lion snack mouse cheetah bat red blue yellow orange black white purple pink violet silver phytoplanton mitochondria supercalifragilistiexpiealadous Sesquipedalian Antidisestablishmentarianism aintbiotics'
+
+
