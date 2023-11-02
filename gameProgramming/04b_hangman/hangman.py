7b1d0d061b002cce97a99320381f7efa9514de8b
Albert <s11641826@students.duvalschools.org>
2023-11-02 14:21:06 -0400

    hangman.py

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -80,6 +80,23 @@ def playAgain():
     print(' Do you want to play again? Yes or No?')
     return input().lower().startswith('y')
 
+# Introduce the game
+print('Welcome to Hangman by Albert L.')
+missedLetters = ''
+correctLetters = ''
+secretWord = getRandomWord(words)
+gameIsDone = False
+
+# MAin Game Loop
+while True:
+    displayBoard(missedLetters, correctLetters, secretWord)
+
+    guess = getGuess(missedLetters + correctLetters)
+
+    if guess in secretWord:
+        correctLetters = correctLetters + guess
+
+        
 
 # i = 0
 # while i < 100:
