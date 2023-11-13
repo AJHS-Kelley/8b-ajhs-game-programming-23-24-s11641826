0586634161c18c8893a11dd762e600d275cedd29
Albert <s11641826@students.duvalschools.org>
2023-11-13 13:50:57 -0500

    exampleGameFunctions.py

--- gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,1 +1,15 @@
 # Example Game FUNCTIONS project, Albert Laguerre, v0.0
+import random
+
+def functionsOne():
+    pass
+
+def functionTwo(param1):
+    pass
+
+def functionThree(param1 = "Default Value"):
+    pass
+
+def functionFour(param1, param2, param3):
+    pass
+
