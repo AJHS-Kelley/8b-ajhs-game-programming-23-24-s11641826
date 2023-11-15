9e650eb75ddbffc21619959a3e4f81ea13c4b00c
Albert <s11641826@students.duvalschools.org>
2023-11-15 14:11:22 -0500

    exampleGameFunctions.py

--- gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,30 +1,18 @@
 # Example Game FUNCTIONS project, Albert Laguerre, v0.0
 import random
 
-def jumpBall():
+def rollDice():
     pass
 
-def dribbleBall(param1):
+def shootDice(param1):
     pass
 
-def shootBall(param1):
+def betAmount(param1):
     pass
 
-def passBall(param1, param2, param3):
+def numberofPlayers(param1, param2, param3):
     pass
 
-def catchBall(ThrowQuality, passCatchScore, weather):
-    if ThrowQuality > 5.0 and passCatchScore >= 99 and weather == 'Sunny':
-        ballCaught = True
-    elif ThrowQuality > 4.0 and passCatchScore >= 75 and weather == 'Windy':
-        ballCaught = False
-    else:
-        print('Oh, no, interceptiopn!\n')
-        ballIntercepted = True
-        return ballIntercepted
-    return ballCaught
-
-catchBall(4.25, 107, 'Rainy')
 
 
 
