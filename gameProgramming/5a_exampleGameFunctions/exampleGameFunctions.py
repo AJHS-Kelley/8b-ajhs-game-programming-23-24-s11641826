29e04854d7d70f07107af9ed9be3e360978733ce
Albert <s11641826@students.duvalschools.org>
2023-11-15 14:20:57 -0500

    exampleGameFunctions.py

--- gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,11 +1,17 @@
 # Example Game FUNCTIONS project, Albert Laguerre, v0.0
 import random
 
+rollDice = 0
+numberofDice = 2
+betAmount = ''
+numberofPlayers = ''
+
 def rollDice():
     pass
 
-def shootDice(param1):
+def numberofDice(param1):
     pass
+    
 
 def betAmount(param1):
     pass
