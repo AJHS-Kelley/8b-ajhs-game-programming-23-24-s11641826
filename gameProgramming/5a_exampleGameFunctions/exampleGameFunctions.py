437ad2a1fcc7390d7afcd964466d3a01f4d5bc6e
Albert <s11641826@students.duvalschools.org>
2023-11-15 14:45:07 -0500

    exampleGameFunctions.py

--- gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -6,8 +6,15 @@ numberofDice = 2
 betAmount = ''
 numberofPlayers = ''
 
-def rollDice():
-    pass
+def rollDice(numDice, sizeDice):
+    numRolled = 0
+    sum = 0
+    while numRolled < numDice:
+        roll = random.randint(1, sizeDice)
+        sum += roll
+        print(f"Roll: {roll}\n")
+        print(f"Sum: {sum}\n")
+        numRolled += 1
 
 def numberofDice(param1):
     pass
