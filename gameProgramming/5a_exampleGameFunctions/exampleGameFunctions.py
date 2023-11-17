3302eba8b087870e4cb47d0bd83b97e9d0a63184
Albert <s11641826@students.duvalschools.org>
2023-11-17 14:24:43 -0500

    exampleGameFunctions.py

--- gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,10 +1,16 @@
 # Example Game FUNCTIONS project, Albert Laguerre, v0.0
 import random
 
-rollDice = 0
-numberofDice = 2
-betAmount = ''
-numberofPlayers = ''
+def main():
+    player1 = 0
+    player1wins = 0
+    player2 = 0
+    player2wins = 0
+    rollDice = 0
+    rounds = 1
+    numberofDice = 2
+    diceRoll = 0
+    
 
 def rollDice(numDice, sizeDice):
     numRolled = 0
@@ -16,14 +22,41 @@ def rollDice(numDice, sizeDice):
         print(f"Sum: {sum}\n")
         numRolled += 1
 
-def numberofDice(param1):
-    pass
-    
-def betAmount(param1):
-    pass
+def numberofDice(numDice, sizeDice):
+    numberofDice = 2
+    sum = 2
 
-def numberofPlayers(param1, param2, param3):
-    pass
+    
+def diceRoll(param1):
+    diceRoll = random.randint(1, 6)
+    return diceRoll
+
+def rounds(param1, param2, param3):
+    print("Round " + str(rounds))
+    player1 = rollDice()
+    player2 = rollDice()
+    print("Player 1 Roll: " + str(player1))
+    print("Player 2 Roll: " + str(player2))
+
+    if player1 == player2:
+        print("Draw!")
+    elif player1 > player2:
+        player1wins = player1wins + 1
+        print("Player 1 wins!")
+    else:
+        player2wins = player2wins + 1
+        print("Player 2 wins!")
+    
+    rounds = rounds + 1 
+
+    if player1wins == player2wins:
+        print("Draw!")
+    elif player1wins > player2wins:
+        print("Player 1 Wins! ")
+    else:
+        print("Player 2 Wins! ")
+    
+
 
 
 
