28e5b74d6568f26830d00e32579328b367f100b2
Albert <s11641826@students.duvalschools.org>
2023-12-14 14:09:41 -0500

    tester.py

--- modules/tester.py
+++ modules/tester.py
@@ -4,9 +4,16 @@ import dice
 roll1 = dice.display(1, 6)
 roll2 = dice.display(1, 6)
 
-if dice.isDoubles(roll1, roll2):
-    print("You rolled a double, go again!\n")
-else:
-    print("It was not a double.\n")
+#if dice.isDoubles(roll1, roll2):
+#    print("You rolled a double, go again!\n")
+#else:
+#    print("It was not a double.\n")
+
+if dice.isExploding(roll1, 6):
+    print(roll1)
+    print("This roll exploded!\n")
+    roll1 += dice.roll(1, 6)
+    print(roll1)
+
 
 
