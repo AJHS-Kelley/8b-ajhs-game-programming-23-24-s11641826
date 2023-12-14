b51ae8b62c2d8b8e912a110f7cd076213c444d7d
Albert <s11641826@students.duvalschools.org>
2023-12-14 14:41:55 -0500

    tester.py

--- modules/tester.py
+++ modules/tester.py
@@ -1,13 +1,10 @@
 import dice
+import perfmon
 
 
 roll1 = dice.display(1, 6)
 roll2 = dice.display(1, 6)
 
-#if dice.isDoubles(roll1, roll2):
-#    print("You rolled a double, go again!\n")
-#else:
-#    print("It was not a double.\n")
 
 if dice.isExploding(roll1, 6):
     print(roll1)
