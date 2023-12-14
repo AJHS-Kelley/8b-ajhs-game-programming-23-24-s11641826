666019bc906c4324c69fbe57a6c081a8c0286dc0
Albert <s11641826@students.duvalschools.org>
2023-12-14 13:36:09 -0500

    dice.py

---
+++ modules/dice.py
@@ -0,0 +1,13 @@
+# Dice rolling module by Albert Laguerre, v0.0
+import random
+
+def rollDice(numDice, sizeDice):
+    numRolled = 0
+    sum = 0
+    while numRolled < numDice:
+        roll = random.randint(1, sizeDice)
+        sum += roll
+        print(f"Roll: {roll}\n")
+        print(f"Sum: {sum}\n")
+        numRolled += 1
+    return sum 
