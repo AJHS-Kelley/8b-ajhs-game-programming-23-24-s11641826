2e2200627123e80a15b32d68f7454149b8aa7b06
Albert <s11641826@students.duvalschools.org>
2023-12-14 14:24:39 -0500

    perfmon.py

---
+++ modules/perfmon.py
@@ -0,0 +1,15 @@
+# Python Performance Monitoring Module by Ryan Kelly, v0.0
+import time
+
+def execStart():
+    startTime = time.time()
+    return startTime
+
+def execStop():
+    stopTime = time.time()
+    return stopTime
+
+def execTime(startTime, stopTime):
+    return f"Execution Time: {startTime - stopTime} seconds.\n"
+
+
