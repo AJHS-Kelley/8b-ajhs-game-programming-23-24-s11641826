4d987e63c99dbe16f9da488959291a450287f7be
Albert <s11641826@students.duvalschools.org>
2024-01-11 14:00:37 -0500

    dnaGame.py

---
+++ gameProgramming/cohort_lessons/dnaGame/dnaGame.py
@@ -0,0 +1,8 @@
+# DNA Replication Game, Ryan Kelly, v0.0
+
+# Import Entired Modules -- Get the whole tool box.
+import time, datetime
+
+# Import Specific Methods -- Get the specific tool.
+from random import choice
+
