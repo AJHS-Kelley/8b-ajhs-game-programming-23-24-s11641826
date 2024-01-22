5dc210cafc7c4bf8d5c52474ca51b3fb83c1ff26
Albert <s11641826@students.duvalschools.org>
2024-01-22 13:51:01 -0500

    numberSlider.py

---
+++ gameProgramming/07_numberSlider/numberSlider.py
@@ -0,0 +1,8 @@
+# Number Slider, by Albert Laguerre, based on a project by AI Sweigart, v0.0
+
+import sys, random, pygame
+# sys module provides access to system resources (i.e Operating system commands)
+
+from pygame.locals import *
+#Allows us to call funcations from pygame using just the funcation name instead of module.functions()
+# Example: We can use draw() instead of pygame.draw()
