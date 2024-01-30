738fbf5ea4ff416c04b1e117bec4dca5e2378f54
Albert <s11641826@students.duvalschools.org>
2024-01-30 14:25:57 -0500

    pygame.py

--- gameProgramming/08_ultimateIntroPygame/ultimateIntroPygame.py
+++ gameProgramming/08_ultimateIntroPygame/ultimateIntroPygame.py
@@ -5,12 +5,18 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
+test_font = pygame.time.Front(None, 50)
+
+test_surface = pygame.Surface((100,200))
+test_surface.fill('')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    # Draw all our elements
-    # update everything
+
+    screen.blit(test_surface,(0,0))
+
     pygame.display.update()
+    clock.tick(60)
