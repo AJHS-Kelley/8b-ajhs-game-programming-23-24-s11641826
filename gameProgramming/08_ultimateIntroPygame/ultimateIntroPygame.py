c02c4567c9c9305e45858b8d0aebf972ba504383
Albert <s11641826@students.duvalschools.org>
2024-01-30 14:40:07 -0500

    pygame.py

--- gameProgramming/08_ultimateIntroPygame/ultimateIntroPygame.py
+++ gameProgramming/08_ultimateIntroPygame/ultimateIntroPygame.py
@@ -5,10 +5,14 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
-test_font = pygame.time.Front(None, 50)
+test_font = pygame.time.Front('font/Pixeltype.ttf', 50)
 
-test_surface = pygame.Surface((100,200))
-test_surface.fill('')
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+text_surface = test_front.render('My game', False, 'Green')
+
+snail_surface = pygame.iamge.load('graphics/snail/snail1.png').convert_alpha()
+snail_x_pos = 600
 
 while True:
     for event in pygame.event.get():
@@ -16,7 +20,12 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(test_surface,(0,0))
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(300,50))
+    snail_x_pos -= 4
+    if snail_x_pos < -100: snail_x_pos = 800
+    screen.blit(snail_surface,(600,250))
 
     pygame.display.update()
     clock.tick(60)
