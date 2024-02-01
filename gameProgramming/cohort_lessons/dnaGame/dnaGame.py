5e939d14920e1898c3d11884c1b292b6d5d2d502
Albert <s11641826@students.duvalschools.org>
2024-02-01 14:49:58 -0500

    dnaGame.py

--- gameProgramming/cohort_lessons/dnaGame/dnaGame.py
+++ gameProgramming/cohort_lessons/dnaGame/dnaGame.py
@@ -1,2 +1,9 @@
 # DNA Replication Game, Albert Laguerre, v0.0
 
+import random
+
+class DNASequence:
+    nucleotides = ['A', 'C', 'G', 'T']
+
+    def __init__(self, length):
+        self.sequence = ''.join(random.choice(self.nucleotides) for _ in range(length))
