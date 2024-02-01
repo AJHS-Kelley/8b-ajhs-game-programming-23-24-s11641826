712fa59f37b0dfd3db3506ee4ec6883b3781e997
Albert <s11641826@students.duvalschools.org>
2024-02-01 14:50:33 -0500

    dnaGame.py

--- gameProgramming/cohort_lessons/dnaGame/dnaGame.py
+++ gameProgramming/cohort_lessons/dnaGame/dnaGame.py
@@ -7,3 +7,17 @@ class DNASequence:
 
     def __init__(self, length):
         self.sequence = ''.join(random.choice(self.nucleotides) for _ in range(length))
+
+def display_sequence(self):
+        return self.sequence
+
+class DNAGame:
+    def __init__(self, length):
+        self.target_sequence = DNASequence(length)
+        self.attempts = 0
+
+    def play(self):
+        print("Welcome to the DNA Guessing Game!")
+        print("Try to guess the DNA sequence.")
+        print("The sequence contains only 'A', 'C', 'G', and 'T'.")
+        print(f"The DNA sequence has a length of {len(self.target_sequence.display_sequence())} bases.")
