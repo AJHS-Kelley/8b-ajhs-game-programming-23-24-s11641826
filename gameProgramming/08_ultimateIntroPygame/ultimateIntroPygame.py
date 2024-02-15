d2d873f7e1410deea1a07d6ae991466c30dadc36
Albert <s11641826@students.duvalschools.org>
2024-02-15 14:45:46 -0500

    pygame.py

--- gameProgramming/08_ultimateIntroPygame/ultimateIntroPygame.py
+++ gameProgramming/08_ultimateIntroPygame/ultimateIntroPygame.py
@@ -48,17 +48,5 @@ while True:
     screen.blit(player_surf,player_rect)
 
 
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_SPACE]:
-    #   print('jump')
-
-    #if player_rect.colliderect(snail_rect):
-    #    print('collision')
-
-    #mouse_pos = pygame.mouse.get_pos()
-    #if player_rect.collidepoint(mouse_pos):
-    #    print(pygame.mouse.get_pressed())
-
-
     pygame.display.update()
     clock.tick(60)
