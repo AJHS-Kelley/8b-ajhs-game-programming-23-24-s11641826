bd14b58009b5073f3da9b6ddaee9045e559cd15c
Albert <s11641826@students.duvalschools.org>
2024-03-27 14:49:36 -0400

    finalProject

---
+++ gameProgramming/09_finalProject/finalProject.py
@@ -0,0 +1,26 @@
+# Final Project, Albert Laguerre, v0.0
+import sys, random, pygame
+
+resolution = 0  # 0 = Low Resolution (800, 600), 1 = High Resolution (1920, 1080)
+
+if resolution == 0:
+    x = 800
+    y = 600
+else:
+    x = 1920
+    y = 1080
+
+pygame.init()
+
+difficulty = int(input("Please choose a difficulty. Enter 1 for EASY or 2 for HARD!"))
+
+pygame.display.set_caption('NAME OF GAME -- EASY')
+pygame.display.set_caption('NAME OF GAME -- HARD')
+
+if difficulty == 1:
+    pygame.display.set_caption('NAME OF GAME -- EASY')
+else:
+    pygame.display.set_caption('NAME OF GAME -- HARD')
+screen = pygame.desplay.set_mode((x, y))
+# CREATE AN if / else BLOCK TO SET RESOLUTION BASED ON THE VARIABLE ABOVE
+
