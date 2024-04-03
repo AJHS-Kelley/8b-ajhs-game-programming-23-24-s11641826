8c948c0be9bfe5319a2d504f560caf410d909ae7
Albert <s11641826@students.duvalschools.org>
2024-04-03 14:03:29 -0400

    sliceDice.py

---
+++ gameProgramming/11_sliceDice/sliceDice.py
@@ -0,0 +1,23 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+myString = "Type a random string with punctuation, please.\n"
+
+print(alphabet[0])
+print(alphabet[-1])
+
+# Slicing
+print(alphabet[0:4]) # Slice up to second index.
+# alphabet[start:stop]
+
+# slice from the start
+print(alphabet[:7])
+
+# slice to the end
+print(alphabet[12:])
+
+# slice the whole %#$#! thing
+print(alphabet[:])
+
+# negavtive index slicing
+print(alphabet[-4:-1])
+
+
