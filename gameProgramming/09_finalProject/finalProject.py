7bb4684dc1ff1affda8a96729befc983fc90db70
Albert <s11641826@students.duvalschools.org>
2024-04-29 12:36:18 -0400

    finalProject.py

--- gameProgramming/09_finalProject/finalProject.py
+++ gameProgramming/09_finalProject/finalProject.py
@@ -24,3 +24,42 @@ font = pygame.font.SysFont(None, 40)
 
 clock = pygame.time.Clock()
 
+def draw_snake(snake_list):
+    for block in snake_list:
+        pygame.draw.rect(screen, green, [block[0], block[1], snake_block, snake_block])
+
+def draw_food(food_x, food_y):
+    pygame.draw.rect(screen, red, [food_x, food_y, food_size, food_size])
+
+def easy_level():
+    snake_list = []
+    snake_length = 1 
+    snake_x = screen_width // 2
+    snake_y = screen_height // 2
+    snake_x_change = 0
+    snake_y_change = 0
+    food_x = round(random.randrange(0, screen_width - food_size) / 20) * 20
+    food_y = round(random.randrange(0, screen_height - food_size) / 20) * 20
+
+    while True:
+        screen.fill((0, 0, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit
+                sys.exit()
+        
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    snake_x_change = -snake_block
+                    snake_y_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    snake_x_change = snake_block
+                    snake_y_change = 0
+                elif event.key == pygame.K_UP:
+                    snake_y_change = -snake_block
+                    snake_x_change = 0
+                elif event.key == pygame.K_DOWN:
+                    snake_y_change = snake_block
+                    snake_x_change = 0
+
