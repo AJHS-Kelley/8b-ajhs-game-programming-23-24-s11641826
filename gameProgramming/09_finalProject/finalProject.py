b1ff1aec5d1a5829f0e78023c97da7a611111490
Albert <s11641826@students.duvalschools.org>
2024-05-01 14:32:08 -0400

    fianlProject.py

--- gameProgramming/09_finalProject/finalProject.py
+++ gameProgramming/09_finalProject/finalProject.py
@@ -1,32 +1,98 @@
 # Fianl Project, Albert Laguerre, v0.0
 
-import math, random, pygame
-import tkinter as tk
-from tkinter import messagebox
+import turtle
+import random
+import time
 
-width = 500
-height = 500
+screen = turtle.Screen()
+screen.title("SNAKE GAME")
+screen.setup(width=700,height=700)
+screen.tracer(0)
+screen.bgcolor("#1d1d1d")
 
-cols = 25
-rows = 20
+turtle.speed(5)
+turtle.pensize(4)
+turtle.penup()
+turtle.goto(-310, 250)
+turtle.pendown()
+turtle.color("red")
+turtle.forward(600)
+turtle.right(90)
+turtle.forward(500)
+turtle.right(90)
+turtle.forward(600)
+turtle.right(90)
+turtle.forward(500)
+turtle.penup()
+turtle.hideturtle()
 
-class cube():
-    rows = 20
-    w = 500
-    def __init__(self, start, dirnx=1, dirny=0, color=(255,0,0)):
-        self.pos = start
-        self.dirnx = dirnx
-        self.dirny = dirny
-        self.color = color
+#score
+score = 0;
+delay = 0.1
 
-    def move(self, dirnx, dirny):
-        self.dirnx = dirnx
-        self.dirny = dirny
-        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+#snake
+snake = turtle.Turtle()
+snake.speed()
+snake.shape("square")
+snake.color("green")
+snake.penup()
+snake.goto(0, 0)
+snake.direction = 'stop'
 
-    def draw(self, surface, eyes = False):
-        dis = self.w // self.rows
-        i = self.pos[0]
-        j = self.pos[1]
+#food
+fruit = turtle.Turtle()
+fruit.speed(0)
+fruit.shape("square")
+fruit.color("white")
+fruit.penup()
+fruit.goto(30, 30)
 
-    
+old_fruit = []
+
+# scoring
+scoring = turtle.Turtle()
+scoring.speed(0)
+scoring.color("white")
+scoring.penup()
+scoring.hideturtle()
+scoring.goto(0,300)
+scoring.write("Score: ", align="center", font=("Courier", 24, "bold"))
+
+#define how to move
+def snake_go_up():
+    if snake.direction != "down":
+        snake.direction = "up"
+
+def snake_go_down():
+    if snake.direction != "up":
+        snake.direction = "down"
+
+def snake_go_left():
+    if snake.direction != "right":
+        snake.direction = "left"
+
+def snake_go_right():
+    if snake.direction != "left":
+        snake.direction = "right"
+
+def move():
+    if snake.direction == "up":
+        y = snake.ycor()
+        snake.sety(y + 20)
+    if snake.direction == "down":
+        y = snake.ycor()
+        snake.sety(y - 20)
+    if snake.direction == "left":
+        x = snake.xcor()
+        snake.sety(x - 20)
+    if snake.direction == "right":
+        x = snake.xcor()
+        snake.sety(x + 20)
+
+
+# keyboard binding
+screen.listen()
+screen.onkeypress(snake_go_up, "Up")
+screen.onkeypress(snake_go_down, "Down")
+screen.onkeypress(snake_go_left, "Left")
+screen.onkeypress(snake_go_right, "Right")
